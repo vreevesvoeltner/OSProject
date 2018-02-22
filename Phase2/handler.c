3a66ef42f4d65059c0552f34f4edfa9d9dbfb43e
Veronica Reeves <Veronica Reeves>
2018-02-22 14:04:42 -0700

    Device handlers made:

--- Phase2/handler.c
+++ Phase2/handler.c
@@ -16,8 +16,10 @@ void nullsys(USLOSS_Sysargs *args)
 
 void clockHandler2(int dev, void *arg)
 {
-   if (DEBUG2 && debugflag2)
-      USLOSS_Console("clockHandler2(): called\n");
+    static int count = 0;
+    int status;
+    if (DEBUG2 && debugflag2)
+        USLOSS_Console("clockHandler2(): called\n");
       
     disableInterrupts();
     if ((USLOSS_PSR_CURRENT_MODE & USLOSS_PsrGet()) == 0) {
@@ -26,18 +28,19 @@ void clockHandler2(int dev, void *arg)
     }
 
     if (dev != USLOSS_CLOCK_DEV) {
-      if (DEBUG2 && debugflag2)
-        USLOSS_Console("clockHandler2(): called by other device, returning\n");
-      return;
+        if (DEBUG2 && debugflag2)
+            USLOSS_Console("clockHandler2(): called by other device, returning\n");
+        return;
     }
-    
-    static int count = 0;
     count++;
     if (count == 5) {
-      int status;
-      USLOSS_DeviceInput(dev, 0, &status); // get the status
-      MboxCondSend(InterruptBoxTable[CLOCK_MBOX], &status, sizeof(int));
-      count = 0;
+        if(USLOSS_DeviceInput(dev, 0, &status) == USLOSS_DEV_INVALID){
+            if (DEBUG2 && debugflag2)
+                USLOSS_Console("clockHandler2(): invalid entry, returning\n");
+            return;
+        }
+        MboxCondSend(InterruptBoxTable[CLOCK_MBOX], &status, sizeof(int));
+        count = 0;
     }
 
     timeSlice();
@@ -47,29 +50,90 @@ void clockHandler2(int dev, void *arg)
 
 void diskHandler(int dev, void *arg)
 {
+    int status;
+    long offset = (long)arg;
+    
+    if (DEBUG2 && debugflag2)
+        USLOSS_Console("diskHandler(): called\n");
+    
+    disableInterrupts();
+    if ((USLOSS_PSR_CURRENT_MODE & USLOSS_PsrGet()) == 0) {
+        USLOSS_Console("disableInterrupts: in user mode. Halting...\n");
+        USLOSS_Halt(1);
+    }
 
-   if (DEBUG2 && debugflag2)
-      USLOSS_Console("diskHandler(): called\n");
-
-
+    if (dev != USLOSS_DISK_DEV) {
+        if (DEBUG2 && debugflag2)
+            USLOSS_Console("diskHandler(): called by other device, returning\n");
+        return;
+    }
+    
+    if(USLOSS_DeviceInput(dev, offset, &status) == USLOSS_DEV_INVALID){
+        if (DEBUG2 && debugflag2)
+            USLOSS_Console("diskHandler(): invalid entry, returning\n");
+        return;
+    }
+    MboxCondSend(InterruptBoxTable[DISK_MBOX + offset], &status, sizeof(int));
+    
+    enableInterrupts();
 } /* diskHandler */
 
 
 void termHandler(int dev, void *arg)
 {
+    int status;
+    long offset = (long)arg;
+    
+    if (DEBUG2 && debugflag2)
+        USLOSS_Console("termHandler(): called\n");
+    
+    disableInterrupts();
+    if ((USLOSS_PSR_CURRENT_MODE & USLOSS_PsrGet()) == 0) {
+        USLOSS_Console("disableInterrupts: in user mode. Halting...\n");
+        USLOSS_Halt(1);
+    }
 
-   if (DEBUG2 && debugflag2)
-      USLOSS_Console("termHandler(): called\n");
-
+    if (dev != USLOSS_TERM_DEV) {
+        if (DEBUG2 && debugflag2)
+            USLOSS_Console("termHandler(): called by other device, returning\n");
+        return;
+    }
+    
+    if(USLOSS_DeviceInput(dev, offset, &status) == USLOSS_DEV_INVALID){
+        if (DEBUG2 && debugflag2)
+            USLOSS_Console("termHandler(): invalid entry, returning\n");
+        return;
+    }
+    MboxCondSend(InterruptBoxTable[TERMINAL_MBOX + offset], &status, sizeof(int));
+    enableInterrupts();
 
 } /* termHandler */
 
 
 void syscallHandler(int dev, void *arg)
 {
+    USLOSS_Sysargs *sysArgs = (USLOSS_Sysargs*) arg;
 
-   if (DEBUG2 && debugflag2)
-      USLOSS_Console("syscallHandler(): called\n");
+    if (DEBUG2 && debugflag2)
+        USLOSS_Console("termHandler(): called\n");
+    
+    disableInterrupts();
+    if ((USLOSS_PSR_CURRENT_MODE & USLOSS_PsrGet()) == 0) {
+        USLOSS_Console("disableInterrupts: in user mode. Halting...\n");
+        USLOSS_Halt(1);
+    }
+    if (dev != USLOSS_SYSCALL_INT) {
+        if (DEBUG2 && debugflag2) 
+            USLOSS_Console("sysCallHandler(): called by other device, returning\n");
+        return;
+    }
 
 
+    if (sysArgs->number < 0 || sysArgs->number >= MAXSYSCALLS) {
+        USLOSS_Console("syscallHandler(): sys number %d is wrong.  Halting...\n", sysArgs->number);
+        USLOSS_Halt(1);
+    }
+
+    nullsys(sysArgs);
+    enableInterrupts();
 } /* syscallHandler */
