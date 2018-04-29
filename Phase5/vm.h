640f551a1b7c118cf379de5e160c736000fcd26c
Veronica Reeves-Voeltner <vreevesvoeltner@email.arizona.edu>
2018-04-29 13:03:17 -0700

    Updated vm.h

--- Phase5/vm.h
+++ Phase5/vm.h
@@ -15,39 +15,65 @@
 #define INCORE 501
 /* You'll probably want more states */
 
+/*
+ * Different states for a data block
+ */
+#define DUNUSED 600
+#define DINUSE  601
+
+/*
+ * Different States for a frame
+ */
+#define FUNUSED 700
+#define FINUSE  701
 
 /*
  * Page table entry.
  */
-typedef struct PTE {
-    int  state;      // See above.
-    int  frame;      // Frame that stores the page (if any). -1 if none.
-    int  diskBlock;  // Disk block that stores the page (if any). -1 if none.
-    // Add more stuff here
-} PTE;
+typedef struct PTE PTE;
+typedef struct PTE* PTEptr;
+typedef struct FTE FTE;
+typedef struct FTE* FTEptr;
+typedef struct Process Process;
+typedef struct Process* ProcPtr;
+typedef struct FaultMsg FaultMsg;
+ 
+struct PTE {
+    int  state,      // See above.
+         frame,      // Frame that stores the page (if any). -1 if none.
+         diskBlock;  // Disk block that stores the page (if any). -1 if none.
+    PTEptr nextPage;
+};
 
 /*
  * Frame table entry
  */
 // Create a struct for your frame table:
+struct FTE{
+    int state,  // Frame state, values to be added above like with PTE
+        frame,  // The number of the frame
+        page,   // The page that uses this frame, -1 if none
+        pid;    // The process number that owns the frame
+    FTEptr nextFrame;
+};
 
 /*
  * Per-process information.
  */
-typedef struct Process {
-    int  numPages;   // Size of the page table.
+struct Process {
+    int  numPages,   // Size of the page table.
+         faultMbox;  // Used to wait for fault resolution
     PTE  *pageTable; // The page table for the process.
-    // Add more stuff here */
-} Process;
+};
 
 /*
  * Information about page faults. This message is sent by the faulting
  * process to the pager to request that the fault be handled.
  */
-typedef struct FaultMsg {
+struct FaultMsg {
     int  pid;        // Process with the problem.
     void *addr;      // Address that caused the fault.
     int  replyMbox;  // Mailbox to send reply.
     // Add more stuff here.
-} FaultMsg;
+};
 
